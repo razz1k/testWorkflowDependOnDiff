e181d4a8d6a18ad0020b6ee3b772ef10f975aafb
razz1k <zdeadmenz@gmail.com>
2025-01-27 01:40:30 +0300

    one entry changed

--- src/entry-1.js
+++ src/entry-1.js
@@ -1,4 +1,4 @@
 import Shared from './_shared/shared';
 import some from './entry-1/some-file';
 console.log("hi from entry-1", Shared, some);
-console.log("new changes 11");
+console.log("new changes 1");
