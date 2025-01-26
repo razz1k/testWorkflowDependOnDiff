7f1dd5bcfb98e5ca3cb31f0287cd1f7303fbeaf3
razz1k <zdeadmenz@gmail.com>
2025-01-27 02:37:24 +0300

    concurrency test

--- src/entry-1.js
+++ src/entry-1.js
@@ -1,4 +1,4 @@
 import Shared from './_shared/shared';
 import some from './entry-1/some-file';
 console.log("hi from entry-1", Shared, some);
-console.log("new changes 12");
+console.log("new changes 1");
